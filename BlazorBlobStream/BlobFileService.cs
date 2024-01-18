b6677c9f01772bc9d299835b4dc8c91453a2646e
Jonatan Matějka <jonatan1024@gmail.com>
2024-01-18 09:05:55 +0100

    improved disposal

--- BlazorBlobStream/BlobFileService.cs
+++ BlazorBlobStream/BlobFileService.cs
@@ -6,19 +6,23 @@ namespace BlazorBlobStream
 {
     public class BlobStreamService : IAsyncDisposable
     {
+        CancellationTokenSource _interopCTS;
         Task<IJSObjectReference> _interopTask;
+
         public BlobStreamService(IJSRuntime jsRuntime)
         {
-            _interopTask = jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BlazorBlobStream/jsInterop.js").AsTask();
+            _interopCTS = new CancellationTokenSource();
+            _interopTask = jsRuntime.InvokeAsync<IJSObjectReference>("import", _interopCTS.Token, "/_content/BlazorBlobStream/jsInterop.js").AsTask();
         }
 
-        public ValueTask<IReadOnlyList<IBrowserFile>> GetBrowserFilesAsync(InputFile inputFile)
-            => GetBrowserFilesAsync(inputFile.Element!.Value);
+        public ValueTask<IReadOnlyList<IBrowserFile>> GetBrowserFilesAsync(InputFile inputFile, CancellationToken cancellationToken = default)
+            => GetBrowserFilesAsync(inputFile.Element!.Value, cancellationToken: cancellationToken);
 
-        public async ValueTask<IReadOnlyList<IBrowserFile>> GetBrowserFilesAsync(ElementReference inputFileElement)
+        public async ValueTask<IReadOnlyList<IBrowserFile>> GetBrowserFilesAsync(ElementReference inputFileElement, CancellationToken cancellationToken = default)
         {
             var interop = await _interopTask;
-            var blobFiles = await interop.InvokeAsync<IReadOnlyList<BrowserFile>>("getFileInfo", inputFileElement);
+
+            var blobFiles = await interop.InvokeAsync<IReadOnlyList<BrowserFile>>("getFileInfo", cancellationToken, inputFileElement);
             foreach (var blobFile in blobFiles)
             {
                 blobFile.ModuleInterop = interop;
@@ -29,10 +33,13 @@ namespace BlazorBlobStream
 
         public async ValueTask DisposeAsync()
         {
+            _interopCTS.Cancel(throwOnFirstException: false);
+
             if (_interopTask.Status == TaskStatus.RanToCompletion)
                 await _interopTask.Result.DisposeAsync();
-
             _interopTask.Dispose();
+
+            _interopCTS.Dispose();
         }
     }
 }
